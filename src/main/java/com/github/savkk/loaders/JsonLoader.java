506fd1cf3f1c8b148a371bff7ca24b2e19651caf
savkk <savkk@bk.ru>
2018-12-14 01:30:07 +0300

    file to string method

--- src/main/java/com/github/savkk/loaders/JsonLoader.java
+++ src/main/java/com/github/savkk/loaders/JsonLoader.java
@@ -4,10 +4,8 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import org.aeonbits.owner.loaders.Loader;
-import org.testng.reporters.Files;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
@@ -28,13 +26,25 @@ public class JsonLoader implements Loader {
     public void load(Properties properties, URI uri) throws IOException {
         File file = new File(uri);
         JsonParser parser = new JsonParser();
-        JsonElement element = parser.parse(Files.readFile(file));
+        JsonElement element = parser.parse(readFile(file));
         jsonElementToProperties(element, properties, "");
     }
 
     public String defaultSpecFor(String uriPrefix) {
         return uriPrefix + ".json";
+    }
 
+    private String readFile(File f) throws IOException {
+        try (InputStream is = new FileInputStream(f)) {
+            StringBuilder result = new StringBuilder();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = br.readLine();
+            while (line != null) {
+                result.append(line).append("\n");
+                line = br.readLine();
+            }
+            return result.toString();
+        }
     }
 
     private void jsonElementToProperties(JsonElement element, Properties properties, String targetKey) {
